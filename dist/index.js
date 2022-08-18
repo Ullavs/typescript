a545316b668836537dcaa7cc794adfaad18a3765
Ullavs <ullavansprang@gmail.com>
2022-08-18 23:10:49 +0200

    practice typescript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log("Hello World");
-let age = 20;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let mySize = 2;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+console.log(kgToLbs(10));
+kgToLbs("10kg");
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,60 @@
-console.log("Hello World");
-let age: number = 20;
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
+// ___________ ENUM _______________
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// _________ FUNCTION _____________
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+calculateTax(10_000);
+
+// _________ OBJECTS ____________
+
+// Type Alias
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Mosh",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// Union types
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+
+console.log(kgToLbs(10));
+kgToLbs("10kg");
+
+// Intersection types
+let weight: number & string;
